32434d6d5e1efb8e0650dcd2abeffda2d394650d
Jared Kerschner <jkerschner@Mower.com>
2017-06-08 07:45:46 -0400

    Set the site to use a custom version of the services widget.

--- bestbuild-child/vc_templates/stm_services_widget.php
+++ bestbuild-child/vc_templates/stm_services_widget.php
@@ -5,7 +5,7 @@ extract( shortcode_atts( array(
 ), $atts ) );
 
 $css_class = apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, vc_shortcode_custom_css_class( $css, ' ' ) );
-$type = 'Stm_Services_Widget';
+$type = 'Stm_Services_Widget_SM';
 $args = array(
 	'before_widget' => '<aside class="widget widget_services wpb_content_element vc_widgets' . $css_class . '">',
 	'after_widget'  => '</aside>',
